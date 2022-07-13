a4b3a48b2b64553d9f03fada785f1f4a58754d39
Dang Khoa <dangkhoa080998@gmail.com>
2022-07-13 09:51:54 +0700

    #init

---
+++ Data/DataContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using GenerateDB.Models;
+
+namespace GenerateDB.Data
+{
+    public partial class DataContext : DbContext
+    {
+        public virtual DbSet<User> User { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=AdminPanel;User ID=sa;Password=P@ssw0rd;Trust Server Certificate=True;Command Timeout=300");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.update_by).IsFixedLength();
+
+                entity.Property(e => e.update_time).IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenerateDB.Models
+{
+    public partial class User
+    {
+        [Key]
+        [StringLength(50)]
+        public string account { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string password { get; set; }
+        [StringLength(50)]
+        public string name { get; set; }
+        [StringLength(50)]
+        public string email { get; set; }
+        public bool is_active { get; set; }
+        [StringLength(10)]
+        public string update_by { get; set; }
+        [StringLength(10)]
+        public string update_time { get; set; }
+    }
+}
